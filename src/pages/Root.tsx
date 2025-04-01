e3cc90212660ebe2a0ada61735adae9b915a9645
Manasa Thota <manasa.manu57@gmail.com>
2025-04-01 13:23:19 +0100

    Added router layout

---
+++ src/pages/Root.tsx
@@ -0,0 +1,9 @@
+import Header from "../componenets/Header"
+import { Outlet } from "react-router-dom"
+
+export default function Root( ) {
+    return <div>
+        <Header/>
+        <Outlet/>
+    </div>
+}
